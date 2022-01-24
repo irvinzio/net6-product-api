51079385a1f63fefb1913e936a89c41f7d877326
Irving Ramirez <irvinzio.ram@gmail.com>
2022-01-23 21:18:41 -0600

    create service interface

---
+++ Tekton.Services/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Tekton.Service.Dto;
+
+namespace Tekton.Service.Interfaces
+{
+    public  interface IProductService
+    {
+        Task<ProductDto> Insert(ProductDto product);
+        Task<ProductDto> Update(Guid id, ProductDto product);
+        Task<ProductDto> Get(Guid id);
+    }
+}
