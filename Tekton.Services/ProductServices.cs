12fba1d31a56b1acaeeea63f95e6dcf4dd15173b
Irving Ramirez <irvinzio.ram@gmail.com>
2022-01-23 21:24:09 -0600

    implement services interface

---
+++ Tekton.Services/ProductServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Tekton.Service.Dto;
+using Tekton.Service.Interfaces;
+
+namespace Tekton.Service
+{
+    public class ProductServices : IProductService
+    {
+        public Task<ProductDto> Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductDto> Insert(ProductDto product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductDto> Update(Guid id, ProductDto product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
