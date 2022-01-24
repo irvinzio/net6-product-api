570423faf7b132bc28fabe2bac90b493eb347a86
Irving Ramirez <irvinzio.ram@gmail.com>
2022-01-24 01:31:59 -0600

    create product services

--- Tekton.Services/ProductServices.cs
+++ Tekton.Services/ProductServices.cs
@@ -1,25 +1,34 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Tekton.Data.Entities;
+using Tekton.Data.Repositories;
 using Tekton.Service.Dto;
 using Tekton.Service.Interfaces;
+using Mapster;
 
 namespace Tekton.Service
 {
     public class ProductServices : IProductService
     {
-        public Task<ProductDto> Get(Guid id)
+        private readonly IRepository<Product> _productRepo;
+        public ProductServices(IRepository<Product> productRepo) { _productRepo = productRepo; }
+        public async Task<ProductDto?> Get(Guid id)
         {
-            throw new NotImplementedException();
+            return  (await _productRepo.Get(id)).Adapt<ProductDto>();
         }
 
-        public Task<ProductDto> Insert(ProductDto product)
+        public async Task<ProductDto> Insert(ProductAddDto productDto)
         {
-            throw new NotImplementedException();
+            var product = productDto.Adapt<Product>();
+            return (await _productRepo.Add(product)).Adapt<ProductDto>();
         }
 
-        public Task<ProductDto> Update(Guid id, ProductDto product)
+        public async Task<ProductDto?> Update(Guid id, ProductDto productDto)
         {
-            throw new NotImplementedException();
+            var product = await _productRepo.Get(id);
+            if(product == null) { return null; }
+            product = productDto.Adapt<Product>();
+            return (await _productRepo.Update(product)).Adapt<ProductDto>();
         }
     }
 }
