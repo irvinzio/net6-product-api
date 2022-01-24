d4edbc7638d1ea98dca9838ea171a836918cb259
Irving Ramirez <irvinzio.ram@gmail.com>
2022-01-24 03:50:42 -0600

    create generic repo implementation

---
+++ Tekton.Data/Repositories/Repository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using Tekton.Data.Context;
+
+namespace Tekton.Data.Repositories
+{
+    public class Repository<T> : IRepository<T>
+        where T : class
+    {
+        private readonly TektonContext context;
+        public Repository(TektonContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<T> Get(Guid id)
+        {
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> Add(T entity)
+        {
+            context.Set<T>().Add(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
