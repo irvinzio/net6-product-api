75cca44cba44f5b268a695c1e6ff4d5aed6ca3bc
Irving Ramirez <irvinzio.ram@gmail.com>
2022-01-23 21:25:30 -0600

    create entitites and repository

---
+++ Tekton.Data/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Tekton.Data.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Master { get; set; }
+        public string Detail { get; set; }
+    }
+}
---
+++ Tekton.Data/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Tekton.Data.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> Get(Guid id);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+    }
+}
