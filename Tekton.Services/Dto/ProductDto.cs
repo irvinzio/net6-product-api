346f8ff58050f3fe1cbce8f900a48e244a0ddf94
Irving Ramirez <irvinzio.ram@gmail.com>
2022-01-23 21:18:54 -0600

    create product dto

---
+++ Tekton.Services/Dto/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tekton.Service.Dto
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Master { get; set; }
+        public string Detail { get; set; }
+    }
+}
