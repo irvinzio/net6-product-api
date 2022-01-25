83ff0e86301e4807eae2cc570ea6fabf4bcbcab1
Irving Ramirez <irvinzio.ram@gmail.com>
2022-01-25 02:47:21 -0600

    apply changes to call mock api

--- Tektok.Infrastructure/Repositories/MockApi/MockApiRepository.cs
+++ Tektok.Infrastructure/Repositories/MockApi/MockApiRepository.cs
@@ -8,10 +8,11 @@ namespace Tektok.Infrastructure.Repositories.MockApiRepo
     public class MockApiRepository: IBaseRepository<MockApiProductModel>, IMockApiRepository
     {
         static HttpClient client = new HttpClient();
+        private readonly string _mockApiUrl = "https://61ef30a5d593d20017dbb369.mockapi.io/tekton/api/v1/product";
         public async Task<MockApiProductModel> Get(int id)
         {
             MockApiProductModel product = null;
-            HttpResponseMessage response = await client.GetAsync("api/products/"+id.ToString());
+            HttpResponseMessage response = await client.GetAsync($"{_mockApiUrl}/{id.ToString()}");
             if (response.IsSuccessStatusCode)
             {
                 product = await response.Content.ReadAsAsync<MockApiProductModel>();
@@ -21,7 +22,7 @@ namespace Tektok.Infrastructure.Repositories.MockApiRepo
 
         public async Task<MockApiProductModel> Add(MockApiProductModel product)
         {
-            HttpResponseMessage response = await client.PostAsJsonAsync("api/products", product);
+            HttpResponseMessage response = await client.PostAsJsonAsync(_mockApiUrl, product);
             response.EnsureSuccessStatusCode();
 
             product = await response.Content.ReadAsAsync<MockApiProductModel>();
@@ -29,7 +30,7 @@ namespace Tektok.Infrastructure.Repositories.MockApiRepo
         }
         public async Task<MockApiProductModel> Update(MockApiProductModel product)
         {
-            HttpResponseMessage response = await client.PutAsJsonAsync($"api/products/{product.Id}", product);
+            HttpResponseMessage response = await client.PutAsJsonAsync($"{_mockApiUrl}/{product.Id}", product);
             response.EnsureSuccessStatusCode();
 
             product = await response.Content.ReadAsAsync<MockApiProductModel>();
