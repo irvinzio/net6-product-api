2afec38c83ead7d566d39b837f7b86ab713f5e3b
Irving Ramirez <irvinzio.ram@gmail.com>
2022-01-26 01:31:42 -0600

    create log for service

--- Tekton.Services/ProductServices.cs
+++ Tekton.Services/ProductServices.cs
@@ -7,6 +7,7 @@ using Tekton.Service.Extensions;
 using LazyCache;
 using Tektok.Infrastructure.MockApi;
 using Tekton.Infrastructure.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace Tekton.Service
 {
@@ -15,6 +16,7 @@ namespace Tekton.Service
         private readonly IRepository<Product> _productRepo;
         private readonly IMockApiRepository _mockApiRepository;
         private readonly IAppCache _cache;
+        private ILogger<ProductService> _logger { get; set; }
 
         public ProductService(IRepository<Product> productRepo, IMockApiRepository mockApiRepository, IAppCache cache) 
         {
@@ -24,9 +26,10 @@ namespace Tekton.Service
         }
         public async Task<ProductDto?> Get(Guid id)
         {
+            _logger.LogInformation($"getting local data for id {id}");
             var product = await _productRepo.Get(id);
             if (product == null) return null;
-
+            _logger.LogInformation($"getting external product data for id {product.ProductMockId}");
             var productsWithCaching = await _cache.GetOrAddAsync(product.Id.ToString(), () => _mockApiRepository.Get(product.ProductMockId));
 
             return product.ToDto(productsWithCaching);
@@ -34,8 +37,9 @@ namespace Tekton.Service
 
         public async Task<ProductDto> Insert(ProductAddDto productDto)
         {
+            _logger.LogInformation($"Inserting external product data for id {productDto}");
             var mockProduct = await _mockApiRepository.Add(productDto.ToMockModel());
-            
+            _logger.LogInformation($"Insrted id {mockProduct.Id}");
             var product = await _productRepo.Add(productDto.ToProduct(mockProduct.Id));
 
             return product.ToDto(mockProduct);
@@ -43,13 +47,17 @@ namespace Tekton.Service
 
         public async Task<ProductDto?> Update(Guid id, ProductDto productDto)
         {
+            _logger.LogInformation($"Verify that product exist with id {id}");
             var product = await _productRepo.Get(id);
 
             if(product == null) { return null; }
 
             product.Detail = productDto.Detail;
 
+            _logger.LogInformation($"Updating local data of product id {id}");
             var updatedProduct = await _productRepo.Update(product);
+
+            _logger.LogInformation($"Updating external api data of product id {updatedProduct.ProductMockId}");
 
             var mockRepo = await _mockApiRepository.Update(updatedProduct.ToMockModel(productDto));
 
